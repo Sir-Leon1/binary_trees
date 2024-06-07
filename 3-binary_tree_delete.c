bbfe4b13bdfd5479225cf2e4bf49348dd9f0cfa7
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-06-07 18:36:05 +0300

    Gitignore

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,7 +9,7 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree != NULL)
 	{
 		binary_tree_delete(tree->left);
-		bianry_tree_delete(tree->right);
+		binary_tree_delete(tree->right);
 		free(tree);
 	}
 }
