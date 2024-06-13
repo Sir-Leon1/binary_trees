7c94e6ec21e3c47ca9e41e9c0f8a4435d2e24e89
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-06-13 15:28:47 +0300

    Task 16 - 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,24 +2,18 @@
 
 /**
  * binary_tree_uncle - Finds the uncle of a node
- * @node: node to find the uncle
+ * @node: Pointer to the node to find the uncle of.
  *
- * Return: The uncle else NULL
+ * Return: If node is NULL or no uncle NULL, Otherwise
+ * the uncle.
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *uncle;
-
-	if (node)
-	{
-		uncle = binary_tree_sibling(node->parent);
-		
-		if (uncle == NULL)
-			return (NULL);
-
-		else
-			return (uncle);
-	}
-
-	return (NULL);
+	if (node == NULL ||
+	    node->parent == NULL ||
+	    node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
